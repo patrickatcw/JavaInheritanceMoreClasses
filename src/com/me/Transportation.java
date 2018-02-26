3ed6fd79930f1975a15456eb7694495910fe9107
patrickatcw <pj8392@yahoo.com>
2018-02-26 15:35:32 -0500

    created fields, constructor and getters in here

--- src/com/me/Transportation.java
+++ src/com/me/Transportation.java
@@ -1,4 +1,20 @@
 package com.me;
 
 public class Transportation {
+
+    private String type;
+    private int wheels;
+
+    public Transportation(String type, int wheels) {
+        this.type = type;
+        this.wheels = wheels;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
 }
