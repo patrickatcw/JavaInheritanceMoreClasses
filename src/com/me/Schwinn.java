e3ac7c157410ef9a2fcee47d3b20fe4c6e0bd543
patrickatcw <pj8392@yahoo.com>
2018-02-26 19:09:55 -0500

    added fields, new constructor and getters

--- src/com/me/Schwinn.java
+++ src/com/me/Schwinn.java
@@ -2,8 +2,25 @@ package com.me;
 
 public class Schwinn extends Bike {
 
+    private int reflectors;
+    private int fenders;
+
     public Schwinn(String type, int wheels, int seats, int handbreaks) {
         super(type, wheels, seats, handbreaks);
 
+    }
+
+    public Schwinn(String type, int wheels, int seats, int handbreaks, int reflectors, int fenders) {
+        super(type, wheels, seats, handbreaks);
+        this.reflectors = reflectors;
+        this.fenders = fenders;
+    }
+
+    public int getReflectors() {
+        return reflectors;
+    }
+
+    public int getFenders() {
+        return fenders;
     }
 }
