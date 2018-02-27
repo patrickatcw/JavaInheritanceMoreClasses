a488bca605e2307828ab4fa0841f4d29a772e5c1
patrickatcw <pj8392@yahoo.com>
2018-02-26 21:23:39 -0500

    created instances for transportation and car

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Transportation transportation = new Transportation("car", 2);
+        System.out.println("One type of transportation is a " + transportation.getType()
+        + " with " + transportation.getWheels() + " wheels.");
+
+        Car car = new Car("Chevy", 2, 2, 1);
+        System.out.println("A type of car could be a " + car.getType() + " with " +
+                car.getWheels() + " wheels, " + car.getDoors() + " doors, and " + car.getTrunk()
+        + " trunk.");
+
     }
 
 }
