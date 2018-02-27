1402037723392077e59f48574a43df3473ef8006
patrickatcw <pj8392@yahoo.com>
2018-02-26 19:02:39 -0500

    created a Scwinn class extending the Bike class

---
+++ src/com/me/Schwinn.java
@@ -0,0 +1,9 @@
+package com.me;
+
+public class Schwinn extends Bike {
+
+    public Schwinn(String type, int wheels, int seats, int handbreaks) {
+        super(type, wheels, seats, handbreaks);
+
+    }
+}
